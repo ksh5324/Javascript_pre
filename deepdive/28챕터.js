97343f07f100d063f78c6997d3305b4b435b298c
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-04-06 17:48:38 +0900

    js

---
+++ deepdive/28챕터.js
@@ -0,0 +1,31 @@
+// const numObj = new Number();
+// console.log(numObj);
+
+// function isEqual(a, b) {
+//   return Math.abs(a - b) < Number.EPSILON;
+// }
+
+// isEqual(0.1 + 0.2, 0.3);
+
+// Number.MAX_SAFE_INTEGER;
+// Number.MIN_SAFE_INTEGER;
+// Number.POSITIVE_INFINITY;
+// Number.NEGATIVE_INFINITY;
+// Number.NaN;
+// Number.isFinite(0);
+// Number.isFinite(Number.MAX_VALUE);
+// Number.isFinite(Infinity);
+// Number.isFinite(-Infinity);
+// Number.isInteger(3);
+// Number.isInteger(0);
+// Number.isInteger(-123);
+// Number.isNaN(undefined);
+
+// (12345.6789).toFixed();
+// (12345.6789).toFixed(1);
+// (12345.6789).toFixed(3);
+
+// (10).toString();
+// (10).toString(2);
+// (10).toString(8);
+// (10).toString(16);
---
+++ deepdive/29챕터.js
@@ -0,0 +1,12 @@
+// Math.PI;
+// Math.abs(-1);
+// Math.abs("-1");
+// Math.abs("");
+
+Math.round(1.4);
+Math.ceil(1.4);
+Math.floor(1.9);
+
+Math.sqrt(9);
+
+Math.random();
