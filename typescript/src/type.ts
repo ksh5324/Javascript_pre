3395a73fa6c11d9ecf8e8fd81b65edd31a0c18fa
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-06-30 10:29:24 +0900

    ts

---
+++ typescript/src/type.ts
@@ -0,0 +1,14 @@
+// function squareOf(n: number) {
+//   return n * n;
+// }
+// squareOf(2);
+// // squareOf("z");
+
+let aa: unknown = 30;
+let bb = aa === 123;
+let cc = aa + 10; // error
+if (typeof aa === "number") {
+  let d = aa + 10;
+}
+
+const p = 1_000_000;
